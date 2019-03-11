f2a91664189c6c400ab615a7e5f7878af8a8957e
davidmuhia87@gmail.com <davidmuhia87@gmail.com>
2019-03-11 13:21:52 +0300

    modified the allowed hosts

--- musicapp/settings.py
+++ musicapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r_j-bszfkqye9)0emi_*#%74@p6agv9wh@k6t)_bskucxf1!r)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
